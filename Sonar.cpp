8804768e9f2f8636ea6c90f9ddc4164b2bf2f36f
KIRIAKE Masanori <seiken@ARGV.org>
2018-05-08 17:23:36 +0900

    4社5乳をしてみた。

--- Sonar.cpp
+++ Sonar.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 #include <time.h>
-const float SoundVelocity=0.017;
+
+const double SoundVelocity=0.017;
 const unsigned int MaxEchoTime=25000;
 class Sonar {
 public:
@@ -28,8 +30,16 @@ Sonar::~Sonar()
 bool Sonar::Ping()
 {
 	// Initial Sensor;
+	// Ping!
+	// Wait pong...
 	if (uSec < MaxEchoTime) {
-		Distance = uSec * SoundVelocity;
+		double RealDistance = uSec * SoundVelocity;
+		int rInt = RealDistance; // 整数部だけ
+		if ((RealDistance-rInt) > 0.5) {
+			rInt++;
+		}
+		// cout << lexical_cast<string>(RealDistance) << std::endl;
+		Distance = rInt;
 		return true;
 	} else {
 		Distance = 0;
@@ -37,10 +47,11 @@ bool Sonar::Ping()
 	}
 }
 
+using std::string;
+using std::cout;
+using std::endl;
 int main()
 {
-	using std::cout;
-	using std::endl;
 	Sonar s;
 	cout << s.getDistance() << endl;
 	return 0;
